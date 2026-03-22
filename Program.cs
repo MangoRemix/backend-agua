35753c91d2f909635cb06b10651d72bc2694fb88
Jesus Castro <jexhanton@gmail.com>
2026-03-22 10:51:57 -0400

    habilitar swagger en prod

--- Program.cs
+++ Program.cs
@@ -90,11 +90,8 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Backend Agua API v1"));
-}
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Backend Agua API v1"));
 
 app.UseHttpsRedirection();
 
