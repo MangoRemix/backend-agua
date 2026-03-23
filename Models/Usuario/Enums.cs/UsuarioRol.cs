dc9f6431441138944ece2de63d6cbda1f81f7927
Jesus Castro <jexhanton@gmail.com>
2026-03-23 08:25:42 -0400

    agregado lider parroquia

--- Models/Usuario/Enums.cs/UsuarioRol.cs
+++ Models/Usuario/Enums.cs/UsuarioRol.cs
@@ -2,9 +2,10 @@ namespace backend_agua.Models;
 
 public enum UsuarioRol
 {
-    Admin,
-    SuperAdmin,
-    Gestor,
-    LiderComunitario,
-    LiderPartido
+    Admin, //0
+    SuperAdmin, //1
+    Gestor, //2
+    LiderComunitario, //3
+    LiderPartido, //4
+    LiderParroquia //5
 }
